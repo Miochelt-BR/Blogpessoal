e03dc96d87a2dc5fdd05b0a38419b257e744fbec
Thiagoarica <thiago.arica@mercadolivre.com>
2022-11-23 15:29:20 -0300

    atualizacao

---
+++ blogpesoal/src/main/java/com/generation/blogpesoal/repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package com.generation.blogpesoal.repository;
+
+import com.generation.blogpesoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+
+}
