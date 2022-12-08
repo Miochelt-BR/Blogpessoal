260c4ad2f17512da8cc69c83256f9d5c638bee3e
Thiagoarica <thiago.arica@mercadolivre.com>
2022-12-08 15:53:37 -0300

    atualizado

--- blogpessoal/blogpessoal/src/main/java/com/pessoal/blogpessoal/controller/UsuarioController.java
+++ blogpessoal/blogpessoal/src/main/java/com/pessoal/blogpessoal/controller/UsuarioController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-public class UsuarioController {
+class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
